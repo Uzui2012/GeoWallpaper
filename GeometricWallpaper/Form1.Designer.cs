aa32f975564786edad64cd2470821da00d4c9d53
Killian <killian2100@hotmail.com>
2020-06-02 02:19:34 +0100

    Added Canvas Panel + Editing line construction algroithm

--- GeometricWallpaper/Form1.Designer.cs
+++ GeometricWallpaper/Form1.Designer.cs
@@ -30,12 +30,13 @@
 		{
 			this.btnNew = new System.Windows.Forms.Button();
 			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.canvasPanel = new System.Windows.Forms.Panel();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// btnNew
 			// 
-			this.btnNew.Location = new System.Drawing.Point(797, 827);
+			this.btnNew.Location = new System.Drawing.Point(610, 855);
 			this.btnNew.Name = "btnNew";
 			this.btnNew.Size = new System.Drawing.Size(266, 134);
 			this.btnNew.TabIndex = 0;
@@ -45,7 +46,7 @@
 			// 
 			// numericUpDown1
 			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(1706, 287);
+			this.numericUpDown1.Location = new System.Drawing.Point(943, 867);
 			this.numericUpDown1.Minimum = new decimal(new int[] {
             20,
             0,
@@ -61,21 +62,30 @@
             0});
 			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
 			// 
+			// canvasPanel
+			// 
+			this.canvasPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.canvasPanel.Location = new System.Drawing.Point(26, 27);
+			this.canvasPanel.Name = "canvasPanel";
+			this.canvasPanel.Size = new System.Drawing.Size(1440, 810);
+			this.canvasPanel.TabIndex = 2;
+			this.canvasPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.canvasPanel_Paint);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1898, 1024);
+			this.Controls.Add(this.canvasPanel);
 			this.Controls.Add(this.numericUpDown1);
 			this.Controls.Add(this.btnNew);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximumSize = new System.Drawing.Size(1920, 1080);
 			this.MinimumSize = new System.Drawing.Size(1920, 1080);
 			this.Name = "Form1";
+			this.ShowIcon = false;
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			this.Text = "GeoWallpaper";
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
 			this.ResumeLayout(false);
 
@@ -85,6 +95,7 @@
 
 		private System.Windows.Forms.Button btnNew;
 		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Panel canvasPanel;
 	}
 }
 
--- GeometricWallpaper/Form1.cs
+++ GeometricWallpaper/Form1.cs
@@ -16,121 +16,106 @@ namespace GeometricWallpaper
 	public partial class Form1 : Form
 	{
 		private int maxDist;
+		
+		Pen pen = new Pen(Color.Black, 1);
+		Graphics g = null;
+
 		public Form1()
 		{
 			InitializeComponent();
+			
 			this.maxDist = Convert.ToInt16(numericUpDown1.Value);
+		}
+
+		private void drawLines()
+		{
+			Point[] points = GeneratePoints();
+			/*
+			String debugMsg = "0:";
+			for (int i = 0; i < points.Length; i++)
+			{
+				debugMsg = debugMsg + points[i] + "\n" + (i + 1);
+			}
+			MessageBox.Show(debugMsg);
+			*/
+			for (int i = 0; i < points.Length; i++)
+			{
+				Point[] nPoints = findNearest5Points(points[i], points);
+				for (int j = 0; j < 5; j++)
+				{
+					String debugMsg = "";
+					for (int k = 0; k < nPoints.Length; k++)
+					{
+						debugMsg = debugMsg + nPoints[k] + "\n" + (k + 1);
+					}
+					MessageBox.Show(debugMsg);
+					g.DrawLine(pen, points[i].X, points[i].Y, nPoints[j].X, nPoints[j].Y);
+				}
+			}
 		}
 
 		private void btnNew_Click(object sender, EventArgs e)
 		{
-			this.Invalidate();
+			canvasPanel.Refresh();
 		}
 
-		private int[][] GeneratePoints()
+		private Point[] GeneratePoints()
 		{
 			Random rand = new Random();
-			int[][] points = new int[1000][];
+			Point[] points = new Point[20];
 			for(int i = 0; i < points.Length; i++){
-				points[i] = new int[2];
-				points[i][0] = rand.Next(1, 101) * 5;
-				points[i][1] = rand.Next(1, 101) * 5;
+				points[i].X = rand.Next(1, 101) * 5;
+				points[i].Y = rand.Next(1, 101) * 5;
 			}
 			return points;
 		}
 
-		private void Form1_Paint(object sender, PaintEventArgs e)
-		{
-			Graphics g = e.Graphics;
-			Pen pen = new Pen(Color.Black, 1);
-			g.DrawLine(pen, 5, 5, 5, 500);
-			g.DrawLine(pen, 5, 5, 995, 5);
-			g.DrawLine(pen, 995, 5, 995, 500);
-			g.DrawLine(pen, 5, 500, 995, 500);
-			int[][] points = GeneratePoints();
+		private Point[] findNearest5Points(Point p, Point[] points){
+			Point[] results = new Point[5];
+			results[0] = points[0];
+			results[1] = points[1];
+			results[2] = points[2];
+			results[3] = points[3];
+			results[4] = points[4];
+			/*
+			String debugMsg = "";
+			for (int i = 0; i < results.Length; i++)
+			{
+				debugMsg = debugMsg + results[i] + "\n" + (i + 1);
+			}
+			MessageBox.Show(debugMsg);			 
+			*/
 			
-			for(int i = 0; i < points.Length; i++){
-				for(int j = 0; j < points.Length; j++){
-					int[] check = { points[i][0], points[i][1], 0 };
-					if(calcDistance(points[i], points[j]) < maxDist){
-						g.DrawLine(pen, points[i][0], points[i][1], points[j][0], points[j][1]);
+			for (int i = 0; i < points.Length; i++){
+				for (int j = 0; j < 5; j++){
+					if(p != points[i]){
+						if(calcDistance(p, points[i]) < calcDistance(p, results[j])){
+							results[j] = points[i];
+						}
 					}
 				}
-				pen.Color = Color.Red;
-				g.DrawLine(pen, points[i][0], points[i][1], points[i][0]+1, points[i][1]+1);
-				pen.Color = Color.Black;
 			}
-		}
-		private double calcDistance(int[] p1, int[] p2)
-		{
-			int dx = p2[0] - p1[0];
-			int dy = p2[1] - p1[1];
-			return Math.Sqrt(Convert.ToDouble(dx*dx + dy*dy));
-		}
 
-		private void Form1_Load(object sender, EventArgs e)
+			return results;
+		}
+		private double calcDistance(Point p1, Point p2)
 		{
-			this.Paint += Form1_Paint;
+			int dx = p2.X - p1.X;
+			int dy = p2.Y - p1.Y;
+			return Math.Sqrt(Convert.ToDouble(dx*dx + dy*dy));
 		}
 
 		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
 		{
 			maxDist = Convert.ToInt16(numericUpDown1.Value);
 		}
-	}
 
-
-
-	public sealed class Wallpaper
-	{
-		Wallpaper() { }
-
-		const int SPI_SETDESKWALLPAPER = 20;
-		const int SPIF_UPDATEINIFILE = 0x01;
-		const int SPIF_SENDWININICHANGE = 0x02;
-
-		[DllImport("user32.dll", CharSet = CharSet.Auto)]
-		static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-
-		public enum Style : int
+		private void canvasPanel_Paint(object sender, PaintEventArgs e)
 		{
-			Tiled,
-			Centered,
-			Stretched
-		}
-
-		public static void Set(Uri uri, Style style)
-		{
-			System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
-
-			System.Drawing.Image img = System.Drawing.Image.FromStream(s);
-			string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-			img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
-
-			RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-			if (style == Style.Stretched)
-			{
-				key.SetValue(@"WallpaperStyle", 2.ToString());
-				key.SetValue(@"TileWallpaper", 0.ToString());
-			}
-
-			if (style == Style.Centered)
-			{
-				key.SetValue(@"WallpaperStyle", 1.ToString());
-				key.SetValue(@"TileWallpaper", 0.ToString());
-			}
-
-			if (style == Style.Tiled)
-			{
-				key.SetValue(@"WallpaperStyle", 1.ToString());
-				key.SetValue(@"TileWallpaper", 1.ToString());
-			}
-
-			SystemParametersInfo(SPI_SETDESKWALLPAPER,
-				0,
-				tempPath,
-				SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+			g = canvasPanel.CreateGraphics();
+			drawLines();
 		}
 	}
+
 }
 
