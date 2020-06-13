2ff710fb213c7057bb82aec54e467c5f367df1c0
Uzui Higgins <killian2100@hotmail.com>
2020-06-13 19:04:28 +0100

    Changed the way I'm to calculate the s-hull algorithm, pushing this to master as transfer to desktop

--- GeometricWallpaper/Form1.cs
+++ GeometricWallpaper/Form1.cs
@@ -16,42 +16,59 @@ namespace GeometricWallpaper
 	public partial class Form1 : Form
 	{
 		private int maxDist;
-		
-		Pen pen = new Pen(Color.Black, 1);
-		Graphics g = null;
+		private Pen pen = new Pen(Color.Black, 1);
+		private Graphics g = null;
 
 		public Form1()
 		{
 			InitializeComponent();
-			
+
 			this.maxDist = Convert.ToInt16(numericUpDown1.Value);
 		}
 
 		private void drawLines()
 		{
-			Point[] points = GeneratePoints();
-			/*
-			String debugMsg = "0:";
-			for (int i = 0; i < points.Length; i++)
+			Point[] points = generatePoints();
+			Point[] seedTriangle = generateTrianglePoints(points);
+			g.DrawPolygon(pen, seedTriangle);
+		}
+
+		private Point[] generateTrianglePoints(Point[] points)
+		{
+			Point x_0 = points[0];
+			Point x_i = points[1];
+			Point x_j = points[1];
+			int curi = 1;
+			for (int i = 1; i < points.Length; i++)
 			{
-				debugMsg = debugMsg + points[i] + "\n" + (i + 1);
-			}
-			MessageBox.Show(debugMsg);
-			*/
-			for (int i = 0; i < points.Length; i++)
-			{
-				Point[] nPoints = findNearest5Points(points[i], points);
-				for (int j = 0; j < 5; j++)
+				if (calcDistance(x_0, points[i]) < calcDistance(x_0, x_i))
 				{
-					String debugMsg = "";
-					for (int k = 0; k < nPoints.Length; k++)
-					{
-						debugMsg = debugMsg + nPoints[k] + "\n" + (k + 1);
-					}
-					MessageBox.Show(debugMsg);
-					g.DrawLine(pen, points[i].X, points[i].Y, nPoints[j].X, nPoints[j].Y);
+					x_i = points[i];
+					curi = i;
 				}
 			}
+			for (int j = 1; j < points.Length; j++)
+			{
+				if (curi != j)
+				{
+					if (calcDistance(x_0, calcCircumCentre(x_0, x_i, points[j])) < calcDistance(x_0, calcCircumCentre(x_0, x_i, x_j)))
+					{
+						x_j = points[j];
+					}
+				}
+			}
+			Point[] result = { x_0, x_i, x_j };
+			return result;
+		}
+
+		private Point calcCircumCentre(Point p1, Point p2, Point p3)
+		{
+			Point circumCenter = new Point();
+			circumCenter.X = ((p1.X * p1.X + p1.Y * p1.Y) * (p2.Y - p3.Y) + (p2.X * p2.X + p2.Y * p2.Y) * (p3.Y - p1.Y) + (p2.X * p3.X + p3.Y * p3.Y) * (p1.Y - p2.Y)) / (2 * (p1.X * (p2.Y - p3.Y) + p2.X * (p3.Y - p1.Y) + p3.X * (p1.Y - p2.Y)));
+			circumCenter.Y = ((p1.X * p1.X + p1.Y * p1.Y) * (p3.Y - p2.Y) + (p2.X * p2.X + p2.Y * p2.Y) * (p1.Y - p3.Y) + (p2.X * p3.X + p3.Y * p3.Y) * (p2.Y - p1.Y)) / (2 * (p1.X * (p2.Y - p3.Y) + p2.X * (p3.Y - p1.Y) + p3.X * (p1.Y - p2.Y)));
+			Rectangle rect = new Rectangle(Convert.ToInt16(circumCenter.X - calcDistance(p1, circumCenter)), Convert.ToInt16(circumCenter.Y-calcDistance(p1, circumCenter)), 2* Convert.ToInt16(calcDistance(p1, circumCenter)), 2*Convert.ToInt16(calcDistance(p1, circumCenter)));
+			g.DrawEllipse(pen, rect);
+			return circumCenter;
 		}
 
 		private void btnNew_Click(object sender, EventArgs e)
@@ -59,50 +76,40 @@ namespace GeometricWallpaper
 			canvasPanel.Refresh();
 		}
 
-		private Point[] GeneratePoints()
+		private Point[] generatePoints()
 		{
 			Random rand = new Random();
 			Point[] points = new Point[20];
-			for(int i = 0; i < points.Length; i++){
-				points[i].X = rand.Next(1, 101) * 5;
-				points[i].Y = rand.Next(1, 101) * 5;
+			List<int> randomXSet = new List<int>();
+			int numX = 0;
+			List<int> randomYSet = new List<int>();
+			int numY = 0;
+			for (int i = 0; i < points.Length; i++)
+			{
+				numX = rand.Next(1, 101);
+				numY = rand.Next(1, 101);
+				while (randomXSet.Contains(numX))
+				{
+					numX = rand.Next(1, 101);
+				}
+				while (randomYSet.Contains(numY))
+				{
+					numY = rand.Next(1, 101);
+				}
+				randomXSet.Add(numX);
+				randomYSet.Add(numY);
+				points[i].X = numX;
+				points[i].Y = numY;
+				g.DrawLine(pen, points[i].X, points[i].Y, points[i].X+1, points[i].Y+1);
 			}
 			return points;
 		}
 
-		private Point[] findNearest5Points(Point p, Point[] points){
-			Point[] results = new Point[5];
-			results[0] = points[0];
-			results[1] = points[1];
-			results[2] = points[2];
-			results[3] = points[3];
-			results[4] = points[4];
-			/*
-			String debugMsg = "";
-			for (int i = 0; i < results.Length; i++)
-			{
-				debugMsg = debugMsg + results[i] + "\n" + (i + 1);
-			}
-			MessageBox.Show(debugMsg);			 
-			*/
-			
-			for (int i = 0; i < points.Length; i++){
-				for (int j = 0; j < 5; j++){
-					if(p != points[i]){
-						if(calcDistance(p, points[i]) < calcDistance(p, results[j])){
-							results[j] = points[i];
-						}
-					}
-				}
-			}
-
-			return results;
-		}
 		private double calcDistance(Point p1, Point p2)
 		{
 			int dx = p2.X - p1.X;
 			int dy = p2.Y - p1.Y;
-			return Math.Sqrt(Convert.ToDouble(dx*dx + dy*dy));
+			return Math.Sqrt(Convert.ToDouble(dx * dx + dy * dy));
 		}
 
 		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
